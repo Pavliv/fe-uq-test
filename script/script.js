b4441c93ddb5c4c273ce0732488fd9d82d74f588
Pavliv <maryan.pavliv@gmail.com>
2017-07-05 14:33:48 +0300

    add burger menu

--- script/script.js
+++ script/script.js
@@ -1,3 +1,44 @@
+// burger menu
+$(document).ready(function(){
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+// display menu
+$("#nav-icon1").click(function () {
+	$(".header li").hide(100);
+	$(".header li").css("display", "block");
+	$(".header li").css("width", "320px");
+	$(".header li").css("text-align", "center");
+	$(".header li").css("background-color", "#fff");
+	$(".header li").css("padding", "10 0");
+	$(".header ul").css("position", "absolute");
+	$(".header ul").css("top", "60");
+	$(".header ul").css("right", "0");
+	$(".dropdown-content").css("display", "none");
+});
+
+// init caroseul
+$(document).ready(function () {
+	var $s = $('.carousel');
+	$s.slick({
+	autoplay: true, 
+	autoplaySpeed: 1000,
+	dots: false,
+	arrows: false,
+	pauseOnFocus: false,
+	pauseOnHover: false,
+	}).on("afterChange", function(e, slick) {
+		if (slick.currentSlide === 1) {
+			$s.slick("setOption", "autoplaySpeed", 7000);
+		} else if (slick.currentSlide > 1) {
+			$s.slick("setOption", "autoplaySpeed", 4000);
+		} else {
+			$s.slick("setOption", "autoplaySpeed", 1000);
+		}
+	});
+});
+
 // init map
 function initMap(){
 	var uluru = { lat: 49.85304056, lng: 24.02387828 };
